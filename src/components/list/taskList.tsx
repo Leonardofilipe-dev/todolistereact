036cea3591662799426c98824beab16880ec3705
Leonardo-dev <leonardo.filipe@yahoo.com>
2023-05-17 15:38:34 -0300

    Finish functions and styles

--- src/components/list/taskList.tsx
+++ src/components/list/taskList.tsx
@@ -1,9 +1,36 @@
+import { ITask } from "../../interface/Task";
+
+import style from "./TaskList.module.css"
+
 
 interface Props {
+  taskList: ITask[]
 }
 
-const taskList = (_props: Props) => {
-  return <div><p>List</p></div>
-};
+const TaskList = ({taskList}: Props) => {
+  return (
+    <>
+    {taskList.length > 0 ? (
+      taskList.map((task) =>(
+        <div key={task.id} className={style.task}>
+        
+          <div className={style.details}>
+          <h5>{task.title}</h5>
+          <p>{task.difficulty}</p>
+          </div>
+          <div className={style.actions}>
+          <i className="bi bi-pencil"></i>
+          <i className="bi bi-trash"></i>
+          </div>
+        </div>
+      ))
+      
+    ): (
+      <p>Não existe tarefas cadastradas!</p>
+    )}
+    </>
+  )
 
-export default taskList;
+    }
+
+export default TaskList
